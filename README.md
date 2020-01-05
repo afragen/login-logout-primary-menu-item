e719592898ad925f5cf7e6e534c807f229edc787
Andy Fragen <andy@thefragens.com>
2020-01-04 18:19:43 -0800

    first pass

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# login-logout-primary-menu-item
-Add a login/logout item to the WordPress primary menu
+# Login Logout Primary Menu Item
+
+* Plugin Name:       Login Logout Primary Menu Item
+* Plugin URI:        https://github.com/afragen/login-logout-primary-menu-item
+* Author:            Andy Fragen
+* Author URI:        http://thefragens.com/
+* Description:       Adds a login/logout menu item to the primary menu.
+* Stable tag:        master
+* License:           MIT
+* GitHub Plugin URI: https://github.com/afragen/login-logout-primary-menu-item
+* Requires PHP:      5.6
+* Requires at least: 4.6
+
+Add a login/logout item to the WordPress primary menu.
---
+++ login-logout-primary-menu-item.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Login Logout Primary Menu Item
+ *
+ * @package login-logout-primary-menu-item
+ * @author Andy Fragen <andy@thefragens.com>
+ * @license MIT
+ * @link https://github.com/afragen/login-logout-primary-menu-item
+ */
+
+/**
+ * Plugin Name:       Login Logout Primary Menu Item
+ * Plugin URI:        https://github.com/afragen/login-logout-primary-menu-item
+ * Author:            Andy Fragen
+ * Author URI:        http://thefragens.com/
+ * Description:       Adds a login/logout menu item to the primary menu.
+ * Version:           0.1.0
+ * Domain Path:       /languages
+ * Text Domain:       login-logout-primary-menu-item
+ * License:           MIT
+ * GitHub Plugin URI: https://github.com/afragen/login-logout-primary-menu-item
+ * Requires PHP:      5.6
+ * Requires at least: 4.6
+ */
+
+add_filter(
+	'wp_nav_menu_items',
+	function ( $items, $args ) {
+		$items .= '<li>' . wp_loginout( 'index.php' ) . '</li>';
+
+		return $items;
+	},
+	10,
+	2
+);
